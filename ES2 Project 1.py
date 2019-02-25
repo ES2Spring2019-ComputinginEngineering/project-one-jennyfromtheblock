69c6b9d45b029817a7c44af69e9f3598323fbb7c
Yassi <yasaman.khorsandian@tufts.edu>
2019-02-25 15:37:07 -0500

    Update ES2 Project 1.py

--- ES2 Project 1.py
+++ ES2 Project 1.py
@@ -1,18 +1,19 @@
 #Project 1: Pedulum
 #Allie and Yassi
 
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 
 g = 9.81
-l = 1
+l = 0.3
 
 def update_system(angAcc,theta,angVel,time1,time2):
     # position and velocity update below
     dt = time2-time1
     newTheta = theta + angVel * dt
     newAngVel = angVel + angAcc*dt
-    newAngAcc = (g/l) * math.cos((math.pi/2) - theta)
+    newAngAcc = (g/l) * math.cos((math.pi/2) - newTheta)
     return newTheta,newAngVel,newAngAcc
 
 def print_system(time,theta,angVel, angAcc):
@@ -25,8 +26,8 @@ def print_system(time,theta,angVel, angAcc):
 # initial conditions
 theta = [(math.pi)/6]
 angVel = [0]
-angAcc = [1]
-time = np.linspace(0,20,21)
+angAcc = [(g/l) * math.cos((math.pi/2) - theta[0])]
+time = np.linspace(0,20,20000)
 print_system(time[0],theta[0],angVel[0],angAcc[0])
 
 for i in range(1, len(time)):
@@ -37,3 +38,12 @@ for i in range(1, len(time)):
     angAcc.append(newAngAcc)
     print_system(time[i],theta[i],angVel[i],angAcc[i])
     i += 1
+
+plt.subplot(3,1,1)
+plt.plot(time, theta, 'ro--')
+
+plt.xlabel('Time (seconds)')
+plt.ylabel('Position (m)')
+plt.title('Position vs Time')
+plt.xlim((0, 20)) # set x range to -1 to 8
+plt.grid()
