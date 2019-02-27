0f676f4aad5f8a4f84c9c39b9e44e9cb844fc4c8
Yassi <yasaman.khorsandian@tufts.edu>
2019-02-27 15:43:08 -0500

    Update DataCollection.py

--- DataCollection.py
+++ DataCollection.py
@@ -5,22 +5,31 @@ from microbit import *
 import math
 import os
 
-myFile = open('/Users/Yasaman/Documents/GitHub/project-one-jennyfromtheblock/angles.txt', 'w')
+myFile = open('angles.txt', 'w')
 angles = []
+time = []
 
 while True:
-    sleep(1000)
-    x = accelerometer.get_x()
-    z = accelerometer.get_z()
-    radX = math.atan2(x, z) #Tilt in x-direction
-    degX = radX / math.pi * 180 #Converting angle of x-tilt from radians to degrees
+    if button_a.is_pressed() == True:
+        time0 = running_time() #get the current running time
+        while True:
+            sleep(50)
+            x = accelerometer.get_x()
+            z = accelerometer.get_z()
+            radX = math.atan2(x, z) #Tilt in x-direction
+            degX = radX / math.pi * 180 #Converting angle of x-tilt from radians to degrees
 
-    if degX > 0:
-        theta = - degX + 180
-    else:
-        theta = - (degX + 180)
+            if degX > 0:
+                theta = - degX + 180
+            else:
+                theta = - (degX + 180)
 
-    angles.append(theta)
-    myFile.write(str(theta))
-    myFile.write("\n")
-    print(angles)
+            time1 = running_time() #get the current running time
+            time.append(time1-time0)
+            angles.append(theta)
+            myFile.write(str(theta))
+            myFile.write("\n")
+            print(angles)
+
+            if button_b.is_pressed():
+                myFile.close()
