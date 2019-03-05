f70bf22b98fd95d729fe1de55daa4e17218e96a5
Yassi <yasaman.khorsandian@tufts.edu>
2019-03-05 17:12:41 -0500

    Update DataAnalysis.py

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -25,10 +25,10 @@ with open ("firstTrial.txt", "r") as file:
         zAcc.append(z_acceleration)
         time.append(t)
 
-#applying filter to the raw data:
-x_filt = sig.medfilt(xAcc)
-z_filt = sig.medfilt(zAcc)
-x_noisy_filt_pks, _ = sig.find_peaks(x_filt)
+##applying filter to the raw data:
+#x_filt = sig.medfilt(xAcc)
+#z_filt = sig.medfilt(zAcc)
+#x_noisy_filt_pks, _ = sig.find_peaks(x_filt)
 
 for i in range(len(zAcc)):
     radX = math.atan2(xAcc[i],zAcc[i])#Tilt in x-direction
@@ -38,20 +38,22 @@ for i in range(len(zAcc)):
 #convert the lists to np.arrays
 t = np.asarray(time)
 theta = np.asarray(angles)
+filt_theta = sig.medfilt(theta,3)
 theta_pks, _ = sig.find_peaks(theta)
+theta_filt_pks, _ = sig.find_peaks(filt_theta)
 
 #Plotting the angle vs. time graph
 plt.subplot(3,1,1)
-plt.plot(time, angles, 'r--') 
+plt.plot(t, filt_theta, 'r-', t[theta_filt_pks],filt_theta[theta_filt_pks] , 'ko')
 plt.xlabel('Time (seconds)')
 plt.ylabel('Angle (degrees)')
 plt.title('Position vs Time')
-plt.xlim((0, 4000)) 
+plt.xlim((500, 4000)) 
 plt.grid()
 
 #Plotting the  acceleration vs. time graph
 plt.subplot(3,1,3)
-plt.plot(time, xAcc, 'r--') 
+plt.plot(time, xAcc, 'r-') 
 plt.xlabel('Time (seconds)')
 plt.ylabel('Acceleration (m/s^2)')
 plt.title('Acceleration vs Time')
@@ -61,7 +63,7 @@ plt.tight_layout()
 
 #Plotting the  acceleration vs. time graph
 plt.subplot(3,1,2)
-plt.plot(time, zAcc, 'b--') 
+plt.plot(time, zAcc, 'b-') 
 plt.xlabel('Time (seconds)')
 plt.ylabel('Acceleration (m/s^2)')
 plt.title('Acceleration vs Time')
@@ -70,7 +72,7 @@ plt.grid()
 plt.tight_layout()
 
 #Determining period by averaging time differences
-peaks = t[theta_pks]
+peaks = t[theta_filt_pks[1:]]
 time_difference = np.diff(peaks)
 period = str(np.sum(time_difference)/len(time_difference))
 print("Period: " + period + "s")
