4952179ecce044b327c7c662a115120ab6290f61
achoi09 <46976966+achoi09@users.noreply.github.com>
2019-03-06 11:25:56 -0500

    Update DataAnalysis.py

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -7,7 +7,7 @@ import scipy.signal as sig
 import math
 
 g = -9.81
-l = 0.365
+l = 0.38
 zAcc = []
 xAcc = []
 time = []
@@ -45,7 +45,7 @@ theta_filt_pks, _ = sig.find_peaks(filt_theta)
 #Plotting the angle vs. time graph
 plt.subplot(3,1,1)
 plt.plot(t, filt_theta, 'r-', t[theta_filt_pks],filt_theta[theta_filt_pks] , 'ko')
-plt.xlabel('Time (seconds)')
+plt.xlabel('Time (milliseconds)')
 plt.ylabel('Angle (degrees)')
 plt.title('Position vs Time')
 plt.xlim((500, 4000)) 
@@ -54,7 +54,7 @@ plt.grid()
 #Plotting the  acceleration vs. time graph
 plt.subplot(3,1,3)
 plt.plot(time, xAcc, 'r-') 
-plt.xlabel('Time (seconds)')
+plt.xlabel('Time (milliseconds)')
 plt.ylabel('Acceleration (m/s^2)')
 plt.title('Acceleration vs Time')
 plt.xlim((500, 4000)) 
@@ -64,7 +64,7 @@ plt.tight_layout()
 #Plotting the  acceleration vs. time graph
 plt.subplot(3,1,2)
 plt.plot(time, zAcc, 'b-') 
-plt.xlabel('Time (seconds)')
+plt.xlabel('Time (milliseconds)')
 plt.ylabel('Acceleration (m/s^2)')
 plt.title('Acceleration vs Time')
 plt.xlim((500, 4000)) 
@@ -74,5 +74,5 @@ plt.tight_layout()
 #Determining period by averaging time differences
 peaks = t[theta_filt_pks[1:]]
 time_difference = np.diff(peaks)
-period = str(np.sum(time_difference)/len(time_difference))
+period = str((np.sum(time_difference)/len(time_difference))/1000)
 print("Period: " + period + "s")
