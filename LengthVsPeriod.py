34c66902decdbc28d577ba1c8e4d0fbd6c925210
Yassi <yasaman.khorsandian@tufts.edu>
2019-03-06 22:02:01 -0500

    Update LengthVsPeriod.py

--- LengthVsPeriod.py
+++ LengthVsPeriod.py
@@ -1,9 +1,11 @@
 #Project 1: Pendulum
+#Allie & Yassi
 #Relationship between pendulum length and period
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+#The values of period and pendulum length as previously calculated
 lengths = np.asarray([0.38, 0.26, 0.28, 0.30, 0.33])
 realPeriods = np.asarray([1.144, 0.832, 1.09, 0.904, 0.968])
 simulatedPeriods = np.asarray([1.262, 1.045, 1.08, 1.122, 1.176])
