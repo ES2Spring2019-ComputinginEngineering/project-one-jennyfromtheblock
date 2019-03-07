a34693bf7087cfa2f17f58ee49795ad308c99277
Yassi <yasaman.khorsandian@tufts.edu>
2019-03-06 21:46:01 -0500

    Update DataCollection.py

--- DataCollection.py
+++ DataCollection.py
@@ -1,31 +1,21 @@
 #Project 1: Pedulum - Data Collection
 #Allie and Yassi
 
-""" first trial: l=0.38m?
-    second trial: l=0.26m
-    third trial: l=0.28m
-    fourth trial: l=0.30m
-    fifth trial: l=0.33m
-"""
 from microbit import *
 import math
 import os
 
 while True:
-
     if button_a.is_pressed() == True:
-        with open('data.txt', 'w') as data:
-            time0 = running_time() #get the current running time
+        with open('data.txt', 'w') as data: #opens the file to record data
+            time0 = running_time() #gets the current running time when button a is pressed
             while True:
-                display.show(Image.HAPPY)
                 sleep(50)
                 x = accelerometer.get_x()
                 z = accelerometer.get_z()
+                time1 = running_time() #gets the current running time
+                data.write(str(time1-time0) + "," + str(x) + "," + str(z)+"\n") #records the time and acceleration values to file
 
-                time1 = running_time() #get the current running time
-                data.write(str(time1-time0) + "," + str(x) + "," + str(z)+"\n")
-
-                if button_b.is_pressed()== True:
-                    display.show(Image.HEART)
+                if button_b.is_pressed()== True: #ends the data collection by closing the file
                     data.close()
                     break
